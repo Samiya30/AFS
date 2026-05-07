edf10bff4e116670c72daf9a209369415eb1ac89
samiya <samiyasharma05@gmail.com>
2026-05-07 12:21:50 +0530

    .

--- firebase-project2/src/App.jsx
+++ firebase-project2/src/App.jsx
@@ -1,4 +1,4 @@
-import { addDoc,getDocs } from 'firebase/firestore'
+import { addDoc,getDocs,doc,getDoc,updateDoc,deleteDoc } from 'firebase/firestore'
 import React from 'react'
 import { collection } from "firebase/firestore";
 import { db } from "./firebase";
@@ -6,14 +6,22 @@ import { db } from "./firebase";
 const App = () => {
 
 async function readDocs() {
-  const data = await getDocs(collection(db, "Blogs"));
   // console.log(data._snapshot.docChanges.forEach(element=>{
   //   console.log(element.doc.data)
   // }))
-  
-  data.forEach((doc) => {
-    console.log(doc.id, doc.data());
+  const data = await getDocs(collection(db, "Blogs"));
+
+  const blogs = data.docs.map((doc) => {
+
+    let blogData = {
+      id: doc.id,
+      ...doc.data()
+    };
+
+    return blogData;
   });
+
+  console.log(blogs);
 }
 
 async function addBlog(title,desc){
@@ -29,11 +37,50 @@ async function addBlog(title,desc){
 }
 }
 
+
+//get a specific blogp
+async function getBlogById(id) {
+  const docRef = doc(db, "Blogs", id);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+
+    const blog = {
+      id: docSnap.id,
+      ...docSnap.data()
+    };
+    console.log(blog);
+    return blog;
+  } else {
+    console.log("No such document!");
+    return null;
+  }
+}
+
+// update a blog
+async function updateBlog(id) {
+  const blogRef = doc(db, "Blogs", id);
+  await updateDoc(blogRef, {
+    title: "Updated Title",
+    desc: "Updated Description"
+  });
+  console.log("Document updated");
+}
+
+// delete a blog
+async function deleteBlog(id) {
+  const blogRef = doc(db, "Blogs", id);
+  await deleteDoc(blogRef);
+  console.log("Document deleted");
+}
+
   return (
     <div>
       <h1>firestore</h1>
       <button onClick={()=>addBlog("first title","blog description")}>addBlog</button>
       <button onClick={readDocs}>Read Data</button>
+      <button onClick={()=>getBlogById("5P4hfrndLRLNmLJnVVLf")}>get blog by id</button>
+      <button onClick={() => updateBlog("5P4hfrndLRLNmLJnVVLf")}>Update Blog</button>
+      <button onClick={() => deleteBlog("5P4hfrndLRLNmLJnVVLf")}>Delete Blog</button>
     </div>
   )
 }
